a521aa7ff86b3b00baeb57fd5d0966ca67aaef14
r.nikolaev <r.nikolaev@smartweb.com.ua>
2017-11-05 13:52:59 +0200

    Initial commit

---
+++ InstaClient/Core/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  InstaClient
+//
+//  Created by Ruslan Nikolaev on 05/11/2017.
+//  Copyright © 2017 Ruslan Nikolaev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return true
+    }
+}
+
